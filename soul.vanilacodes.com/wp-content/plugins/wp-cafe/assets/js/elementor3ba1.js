020127bbd152ce356c23450876b7e3ea83224acf
pantu001 <subhadeep1998saha@gmail.com>
2021-05-04 15:14:42 +0530

    all files added

---
+++ soul.vanilacodes.com/wp-content/plugins/wp-cafe/assets/js/elementor3ba1.js
@@ -0,0 +1,1 @@
+(function($,elementor){"use strict";var wpc={init:function(){var widgets={'wpc-menu-tab.default':wpc.wpc_menu_tab,};$.each(widgets,function(widget,callback){elementor.hooks.addAction('frontend/element_ready/'+widget,callback);});},};$(window).on('elementor/frontend/init',wpc.init);}(jQuery,window.elementorFrontend));
